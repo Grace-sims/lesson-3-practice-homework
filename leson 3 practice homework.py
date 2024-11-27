fcff5a40b803f5c003549edd8558f91ea84e1b2a
Grace-sims <gracesims2012@gmail.com>
2024-11-27 19:55:12 +0700

    Add files via upload

---
+++ leson 3 practice homework.py
@@ -0,0 +1,21 @@
+for x in range (1,21,1):
+    print (x)
+
+print ()
+
+y = 1
+while y<=20 :
+    print (y)
+    y = y + 1
+
+print ()
+
+for x in range (50,4,-5):
+    print (x)
+
+print ()
+
+y = 50
+while y>=5:
+    print (y)
+    y = y - 5
